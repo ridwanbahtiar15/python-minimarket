04f1615ebadc1ae0b5feeea36b9156b4dbc5ebb0
Ridwan Bahtiar <133017656+ridwanbahtiar15@users.noreply.github.com>
2025-01-18 02:50:45 +0700

    Update index add mysql.py

--- index.py
+++ index.py
@@ -1,15 +1,17 @@
 from tabulate import tabulate
+import mysql.connector
 
-data = [
-  ['pr01', 'Oreo', 10000],
-  ['pr02', 'Soft Cookies', 12000],
-  ['pr03', 'Mie Goreng', 4000],
-]
+cnx = mysql.connector.connect(user='root', password='', host='localhost', database='minimarket')
+cursor = cnx.cursor()
+
+sql = ('SELECT * FROM product')
+cursor.execute(sql)
+data = cursor.fetchall()
 
 print('')
-print('-' * 12, 'List Produk', '-' * 12)
+print('-' * 16, 'List Produk', '-' * 16)
 print('')
-print(tabulate(data, headers=['Kode Produk', 'Nama Produk', 'Harga']))
+print(tabulate(data, headers=['Kode Produk', 'Nama Produk', 'Harga', 'Stok']))
 print('')
 print('* Belanja lebih dari Rp.50000 dan dapatkan potongan 10% *')
 print('')
@@ -20,16 +22,25 @@ loop = True
 dataExisting = False
 
 def searchProduct(productId):
-  for i in range(len(data)):
-    if productId == data[i][0]:
-      return True, i
-  return False, None
+  sqlFetch = "SELECT * FROM product WHERE product_id = %s"
+  cursor.execute(sqlFetch, [productId])
+  return cursor.fetchone()
 
 def addProduct(productId, productQty):
-  check, i = searchProduct(productId)
-  if check:
-    result.append([data[i][1], data[i][2], productQty, data[i][2] * productQty])
-    return data[i][2] * productQty
+  data = searchProduct(productId)
+  if data:
+    if data[3] == 0 or data[3] < productQty:
+      return 0
+    newStock =  data[3] - productQty
+    result.append([data[0], data[1], productQty, data[2] * productQty])
+    sqlUpdate = "UPDATE product SET stock = %s WHERE product_id = %s"
+    val = (newStock, productId)
+    try:
+      cursor.execute(sqlUpdate, val)
+      cnx.commit()
+      return data[2] * productQty
+    except:
+      cursor.rollback()
 
 def discountFunc(price):
   return int(price * 0.1)
@@ -37,8 +48,8 @@ def discountFunc(price):
 while loop:
   while not dataExisting:
     productId = input('Masukkan kode produk : ')
-    check, _ = searchProduct(productId)
-    if check:
+    check = searchProduct(productId)
+    if check != None:
       dataExisting = True
     else:
       print('Kode Produk tidak ditemukan. Coba lagi')
@@ -53,7 +64,12 @@ while loop:
       print('Masukkan angka yang valid. Coba lagi')
 
   if dataExisting:
-    total += addProduct(productId, productQty)
+    subtotal = addProduct(productId, productQty)
+    if subtotal != 0:
+      total += subtotal
+    else:
+      dataExisting = False
+      print('Stok tidak tersedia')
 
   while dataExisting:
     isAddProduct = input('Apakah ingin menambah produk? [y/t] : ')
@@ -74,3 +90,5 @@ if total > 50000:
   print(f'* Anda mendapat potongan 10%. diskon Rp. {discountFunc(total)}')
   total = total - discountFunc(total)
 print(f'Total yang harus dibayar : Rp. {total}')
+
+cnx.close()
